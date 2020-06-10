6da43f183c0382f73739807682c7ed012f9f48f9
CentipedeKid <reidcrafter@gmail.com>
2020-06-09 22:09:31 -0400

    ravenous day 3

--- src/Components/BusinessList/BusinessList.js
+++ src/Components/BusinessList/BusinessList.js
@@ -1,17 +1,20 @@
-import React from "react";
-import "./BusinessList.css";
-import Business from "../Business/Business";
+import React from 'react';
+import './BusinessList.css';
 
-function BusinessList() {
-  return (
-    <div className="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-    </div>
-  );
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
+  }
 }
+
 export default BusinessList;
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,16 +4,17 @@ import "./SearchBar.css";
 const sortByOptions = {
   "Best Match": "best_match",
   "Highest Rated": "rating",
-  "Most reviewed": "review_count",
+  "Most Reviewed": "review_count",
 };
 
 class SearchBar extends React.Component {
   renderSortByOptions() {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}></li>;
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
     });
   }
+
   render() {
     return (
       <div className="SearchBar">
